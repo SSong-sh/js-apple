7b946643c030aeb4ecafba6913cddde07292ad27
seonghye <tjdgp0817@gmail.com>
2023-06-16 20:32:05 +0900

    Added dataset

--- tab.js
+++ tab.js
@@ -1,10 +1,18 @@
-for (let i = 0; i < $(".tab-button").length; i++) {
-  $(".tab-button")
-    .eq(i)
-    .on("click", function () {
-      $(".tab-button").removeClass("orange");
-      $(".tab-button").eq(i).addClass("orange");
-      $(".tab-content").removeClass("show");
-      $(".tab-content").eq(i).addClass("show");
-    });
+// for (let i = 0; i < $(".tab-button").length; i++) {
+//   $(".tab-button")
+//     .eq(i)
+//     .on("click", function () {
+//       탭열기(i);
+//     });
+// }
+
+$(".list").click(function (e) {
+  탭열기(parseInt(e.target.dataset.id));
+});
+
+function 탭열기(num) {
+  $(".tab-button").removeClass("orange");
+  $(".tab-button").eq(num).addClass("orange");
+  $(".tab-content").removeClass("show");
+  $(".tab-content").eq(num).addClass("show");
 }
